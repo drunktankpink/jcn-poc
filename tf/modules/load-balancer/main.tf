6242259aa83a995432b3b27746b7359bf16e821a
dtp <37308847+drunktankpink@users.noreply.github.com>
2022-12-20 09:40:57 +0000

    Update main.tf

--- tf/modules/load-balancer/main.tf
+++ tf/modules/load-balancer/main.tf
@@ -22,26 +22,20 @@ resource "aws_lb" "this" {
   subnets            = var.subnets
   security_groups    = var.load_balancer_type == "application" ? [var.security_groups] : null
   
-  idle_timeout                     = var.idle_timeout
-  enable_cross_zone_load_balancing = var.enable_cross_zone_load_balancing
-  enable_deletion_protection       = var.enable_deletion_protection
-  enable_http2                     = var.enable_http2
-  ip_address_type                  = var.ip_address_type
-  
   tags = {
     Name = "${var.service_name}-${local.location_abr}-lb"
   }
 }
-
+  
 resource "aws_lb_target_group" "target_group" {
   name        = "${var.service_name}-${local.location_abr}-tg"
   port        = var.lb_port
-  protocol    = local.lb_protocol
+  protocol    = var.lb_protocol
   vpc_id      = var.vpc_id
   target_type = var.target_type
 
     health_check {
-      protocol            = local.lb_protocol
+      protocol            = var.lb_protocol
       port                = var.lb_port
       interval            = 10
       matcher             = var.load_balancer_type == "application" ? "200" : null
@@ -50,14 +44,13 @@ resource "aws_lb_target_group" "target_group" {
   }
   tags = {
     Name        = "${var.service_name}-${local.location_abr}-lb"
-    Environment = var.resource_environment
   }
 }
 
 resource "aws_lb_listener" "front_end" {
   load_balancer_arn = aws_lb.this.arn
   port              = var.lb_port
-  protocol          = local.lb_protocol
+  protocol          = var.lb_protocol
 
   default_action {
     type             = "forward"
@@ -66,8 +59,7 @@ resource "aws_lb_listener" "front_end" {
 }
 
 resource "aws_lb_target_group_attachment" "this" {
-  count            = length(var.instance_ids)
   target_group_arn = aws_lb_target_group.target_group.arn
-  target_id        = var.instance_ids[count.index]
+  target_id        = var.ecs_target_id
   port             = var.lb_port
 }
